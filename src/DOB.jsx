7867b33985ef000a94d0e980305e8f2d094916d7
UMANANDESWARI <umanandeswarikunisetti17@gmail.com>
2025-09-03 12:09:22 +0530

    Form Changes

---
+++ src/DOB.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const DOB =() =>{
+    return(
+        <center>
+        <form>
+            <label> DateOfBirth </label>
+            <input type="date" placeholder="Enter your DOB" />
+            <br/><br />
+        </form>
+        </center>
+    )
+}
+export default DOB;
---
+++ src/Gender.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Gender =() =>{
+    return(
+        <center>
+        <form>
+            <label> Gender </label>
+            <input type="radio" name="gender" />
+            <label> Male </label>
+            <input type="radio" name="gender" />
+            <label> Female </label>
+            <br/><br />
+        </form>
+        </center>
+    )
+}
+export default Gender;
