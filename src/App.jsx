a71c9d0b6facaf090cfe8edceb1e8830445125b0
UMANANDESWARI <umanandeswarikunisetti17@gmail.com>
2025-09-03 11:18:59 +0530

    React Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ClassComponent from './ClassComponent'
+import FunctionComponent from './FunctionComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ClassComponent />
+      <FunctionComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/ClassComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+class ClassComponent extends Component{
+    render(){
+        return(
+            <h1>Component</h1>
+        )
+    }
+}
+export default ClassComponent;
