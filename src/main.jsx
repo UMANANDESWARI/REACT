f9b500372ffe9cb44d55491779c6a70bae905ebb
UMANANDESWARI <umanandeswarikunisetti17@gmail.com>
2025-09-11 20:59:44 +0530

    react commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Component1 from './Component1'
 // import './index.css'
-// import App from './App.jsx'
-import CLASS from './Cards.jsx'
+import App from './App.jsx'
+// import CLASS from './Cards.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <CLASS />
+    <App />
+    {/* <CLASS /> */}
   </StrictMode>,
 )
