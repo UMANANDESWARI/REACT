d6eec238c9559f04928fd279d174e8fa15a5b4df
UMANANDESWARI <umanandeswarikunisetti17@gmail.com>
2025-10-06 19:53:27 +0530

    react commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () =>{
     return(
         <>
             <h1>Hello! You're in Home page</h1>
+            <Link to= "/Services" >Click Me</Link>
         </>
     )
 }
