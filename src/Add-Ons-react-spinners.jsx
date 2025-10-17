fe1001d5c202efcb63e1a4d6992bf0a34fff3eaa
UMANANDESWARI <umanandeswarikunisetti17@gmail.com>
2025-10-17 11:00:34 +0530

    react commit

---
+++ src/Add-Ons-react-spinners.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { DNA } from "react-loader-spinner";
+function AddOn_spinner(){
+    return(
+        <>
+        <h1>DNA Loading</h1>
+        <DNA />
+        </>
+    )
+}
+export default AddOn_spinner
