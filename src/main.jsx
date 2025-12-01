7969d1455b00b985fb3cf07c7e11b944aafc83d0
UMANANDESWARI <umanandeswarikunisetti17@gmail.com>
2025-12-01 20:47:00 +0530

    react commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,37 @@ import Routing from './Routing.jsx'
 import ProductCards from './ProductCards.jsx'
 import ImagePaths from './ImagePaths.jsx'
 import ImagePaths2 from './ImagePaths2.jsx'
+import Task1 from './Task1.jsx'
+import AddOn_countup from './Add-Ons-react-countup.jsx'
+import AddOn_tilty from './Add-Ons-react-tilty.jsx'
+import AddOn_toaster from './Add-Ons-react-toaster.jsx'
+import AddOn_spinner from './Add-Ons-react-spinners.jsx'
+import AddOn_confetti from './Add-Ons-react-confetti.jsx'
+import Task from './Task.jsx'
+import ContextTaskC1 from './ContextTaskC1';
+import Typing_Task from './Typing_Task.jsx'
+import Dashboard from './Dashboard.jsx'
+import Dashboard_Task from './Dashboard_Task.jsx'
+import FitnessDashboard from './FitnessDashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Routing />
+    {/* <Routing /> */}
     {/* < ImagePaths/> */}
     {/* <ImagePaths2 /> */}
     {/* <CLASS /> */}
+    {/* <Task1 /> */}
+    {/* <AddOn_countup /> */}
+    {/* <AddOn_tilty /> */}
+    {/* <AddOn_toaster /> */}
+    {/* <AddOn_spinner /> */}
+    {/* <AddOn_confetti /> */}
+    {/* <Task /> */}
+    {/* <ContextTaskC1 /> */}
+    {/* <Typing_Task /> */}
+    {/* < Dashboard/> */}
+    {/* < Dashboard_Task/> */}
+    < FitnessDashboard/>
   </StrictMode>,
 )
